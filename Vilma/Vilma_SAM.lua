56e51c4d23d3387712b77a97341d67aa3d49defa
Ben Johnson <bwjohnson92@gmail.com>
2025-12-11 14:46:27 +0000

    Automatic update

--- Vilma/Vilma_SAM.lua
+++ Vilma/Vilma_SAM.lua
@@ -15,7 +15,7 @@ function get_sets()
     sets.AF.Body = "Wakido Domaru"
     sets.AF.Head = "Wakido Kabuto +1"
 
-    sets.Relic.Feet = "Sakonji Sune-Ate +3"
+    sets.Relic.Feet = "Sakonji Sune-Ate +4"
     sets.Relic.Legs = "Sakonji Haidate"
     sets.Relic.Hands = "Sakonji Kote +2"
     sets.Relic.Body = "Sakonji Domaru +4"
